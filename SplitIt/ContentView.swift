6b37255522432edf8fd0378746c35f9d8c2cb47a
Tulitu Vlad <tulituvlad@gmail.com>
2023-03-12 23:18:14 +0200

    Updated UI

--- SplitIt/ContentView.swift
+++ SplitIt/ContentView.swift
@@ -2,10 +2,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack{
+        NavigationView{
+            Form{
+                Section{
+                    TextField("Amount",value: 0,format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                    Picker("Number of people",selection: 0){
+                        ForEach(2..<100){
+                            Text("\($0) people")
+                        }
+                    }
+                }
+            }
             
-        }
-       
+        }.navigationTitle("SplitIt")
     }
 }
 
