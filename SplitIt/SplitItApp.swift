e153933bf0b49e6991fce99f68b0a5d2d732b95e
Tulitu Vlad <tulituvlad@gmail.com>
2023-03-12 23:07:19 +0200

    Initial Commit

---
+++ SplitIt/SplitItApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitItApp.swift
+//  SplitIt
+//
+//  Created by Vlad on 3/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SplitItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
