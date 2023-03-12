99d3a2933b70d2938acef8e41c19933f8a639d71
Tulitu Vlad <tulituvlad@gmail.com>
2023-03-12 23:18:14 +0200

    Updated UI

--- SplitIt/ContentView.swift
+++ SplitIt/ContentView.swift
@@ -1,11 +1,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var checkAmount=0.0
+    @State private var numberOfPeople=2
+    @State private var tipPercentage=20
+    let tipPercentages=[10,15,20,25,0]
+    
     var body: some View {
-        VStack{
+        NavigationView{
+            Form{
+                Section{
+                    TextField("Amount",value: $checkAmount,format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                    Picker("Number of people",selection: $numberOfPeople){
+                        ForEach(2..<100){
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                Section{
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                    Button("Other"){
+                        
+                    }
+                }header: {
+                    Text("How much tip do you want to leave?")
+                }
+                
+            }
             
-        }
-       
+        }.navigationTitle("SplitIt")
     }
 }
 
